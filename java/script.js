e1065b13ec0d9544f3fc826babe8544a33f6229c
shea13pena <126898888+shea13pena@users.noreply.github.com>
2023-09-11 17:07:24 -0400

    heavy update

---
+++ java/script.js
@@ -0,0 +1,14 @@
+const sidebar = document.querySelector('.sidebar');
+const content = document.querySelector('.content');
+
+// Add an event listener to the window for scrolling
+
+// window.addEventListener('scroll', () => {
+    // Calculate the scroll position
+    
+    //const scrollPosition = window.scrollY;
+
+    // Set the top position of the sidebar to match the scroll position
+    
+    //sidebar.style.top = scrollPosition + 'px';
+// });
